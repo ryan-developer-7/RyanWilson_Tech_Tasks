5649170ef0f9a29e17fb30b4d61f265b39fd7fcc
Ryan Wilson <ryan_hughes_smith@live.co.uk>
2021-02-17 09:08:28 +0000

    LWC components are created

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,3 +1,13 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class ChildComponent extends LightningElement {}
+export default class ChildComponent extends LightningElement {
+  @api inputitem;
+
+  fireSelectEvent = () => {
+    const selectedEvent = new CustomEvent("inputitemselect", {
+      detail: this.inputitem,
+      bubbles: true
+    });
+    this.dispatchEvent(selectedEvent);
+  };
+}
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,3 +1,26 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
-export default class ParentComponent extends LightningElement {}
+export default class ParentComponent extends LightningElement {
+  @track input;
+  @track list = Array();
+  @track selected;
+
+  constructor() {
+    super();
+    addEventListener("inputitemselect", this.handleSelect);
+  }
+
+  addInputToList = () => {
+    let inputValue = this.template.querySelector("lightning-input").value;
+
+    if (inputValue.length > 0) {
+      this.list.push(inputValue);
+      this.template.querySelector("lightning-input").value = "";
+    }
+  };
+
+  handleSelect = (event) => {
+    this.selected = true;
+    this.input = event.detail;
+  };
+}
