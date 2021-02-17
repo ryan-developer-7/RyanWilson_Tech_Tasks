bedc5f4833bb4dad1909d9b0d2cb904b7a0def6c
Ryan Wilson <ryan_hughes_smith@live.co.uk>
2021-02-17 09:26:38 +0000

    Adding comments.

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,7 +1,15 @@
 import { LightningElement, api } from "lwc";
 
 export default class ChildComponent extends LightningElement {
+  /* We use the api decorator to make the property public 
+  and important for the parentComponent to insert the inputItem text.
+  */
   @api inputitem;
+
+  /* This method fires an event up to the parent component. 
+    When this iteration of the child component is selected.
+    This is used for the hidden text in parent template. 
+  */
 
   fireSelectEvent = () => {
     const selectedEvent = new CustomEvent("inputitemselect", {
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,15 +1,20 @@
 import { LightningElement, track } from "lwc";
 
 export default class ParentComponent extends LightningElement {
+  //These below properties need the track decorator so that the template renders when they change.
   @track input;
   @track list = Array();
   @track selected;
 
+  // We require this constructor for adding the event listener.
   constructor() {
     super();
     addEventListener("inputitemselect", this.handleSelect);
   }
 
+  /* This method is used for adding items to the list. 
+    It also sets the input value back to blank
+  */
   addInputToList = () => {
     let inputValue = this.template.querySelector("lightning-input").value;
 
@@ -19,6 +24,9 @@ export default class ParentComponent extends LightningElement {
     }
   };
 
+  /* This method handles the event that bubbles up from the child component. 
+   It allows the template to show the select input text.
+  */
   handleSelect = (event) => {
     this.selected = true;
     this.input = event.detail;
